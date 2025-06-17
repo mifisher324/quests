7374839d700f591a569cf683bc213d699fdad891
Convection <mfish1@gmail.com>
2025-06-17 00:30:30 -0500

    Rewrite Ancient Cragbeast Matriarch to work on THJ

--- txevu/encounters/acm.lua
+++ txevu/encounters/acm.lua
@@ -1,95 +1,190 @@
---[[
---  Ancient Cragbeast Matriarch
---  NPCs involved:
---      #Ancient_Cragbeast_Matriarch (297056)
---      #Bearer_of_Absorption (297173)
---      #Bearer_of_Anger (297055)
---      #Bearer_of_Haste (297190)
---      #Bearer_of_Intensification (297051)
---      #Bearer_of_Mending (297054)
---      #Bearer_of_Nascency (297050)
---      #Bearer_of_Projection (297208)
---      #Bearer_of_Quickening (297142)
---      #Bearer_of_Rage (297053)
---      #Bearer_of_Reflection (297052)
---      #Bearer_of_Resistance (297141)
---      #Bearer_of_Shielding (297191)
---  http://everquest.allakhazam.com/db/quest.html?quest=7691
---]]
+--Ancient Cragbeast Matriarch (THJ)
+local thread_manager = require("thread_manager")
 
-local ae			= false
-local adds			= false
-local combat		= false
-local instance_id	= eq.get_zone_instance_id();
-local bearer_list	= {297173,297055,297190,297051,297054,297050,297208,297142,297053,297052,297141,297191};
+local bearer_list = {297173, 297055, 297190, 297051, 297054, 297050, 297208, 297142, 297053, 297052, 297141, 297191}
+local handler = 297048 
+local matriarch = 297056
+local hatchling = 297210
 
-function ACMSpawn(e)
-	local entity_list = eq.get_entity_list()
-	ae = false
-	adds = false
-	combat = false
-	-- due to the way spawn conditions work, if on boot up the condition is
-	-- enabled the mob spawn order is not guaranteed
-	-- This isn't as clean as I would like, but it's better than checking them all on a timer
+local bearer_sw = {669.00, -115.00, -351.52, 0}
+local bearer_s = {641.00, -115.00, -351.52, 0}
+local bearer_se = {615.00, -115.00, -350.9, 0}
+local bearer_nw = {669.00, 115.00, -353.9, 256}
+local bearer_n = {641.00, 115.00, -353.9, 256}
+local bearer_ne = {615.00, 115.00, -353.9, 256}
+local handler_n = {641.00, 80.00, -364.9, 256}
+local handler_s = {641.00, -80.00, -360.27, 0}
+local acm = {627.00, 0.00, -364.77, 118}
 
-	for i = 1, #bearer_list do
-		if entity_list:IsMobSpawnedByNpcTypeID(bearer_list[i]) then
-			eq.signal(297056, bearer_list[i]);
-		end
-	end
-	eq.set_timer("spawnevent", 3000)
+local sw = 0
+local s = 0
+local se = 0
+local nw = 0
+local n = 0
+local ne = 0
+
+local adds = false
+local combat = false
+local ae = false
+
+function pick_bearer(locs)
+	--Pick a random bearer
+  local idx = math.random(#bearer_list)
+  local spawn_id = table.remove(bearer_list, idx)
+  eq.spawn2(spawn_id, 0, 0, unpack(locs))
+  return spawn_id
+end
+
+function reset()
+	--Despawn everything
+	eq.depop_all(handler)
+	eq.depop_all(hatchling)
 	
-end
-
-function ACMDeath(e)
-	eq.spawn_condition("txevu", instance_id, 3, 0) -- bearers and Mastruqs
-	ae = false
-	adds = false
-	combat = false
-end
-
-function ACMTimer(e)
-	if e.timer == "hatchling" then
-		local x,y,z,h = e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading();
-		eq.zone_emote(MT.Yellow, "In search of food, cragbeast hatchlings scurry out from beneath the Ancient Cragbeast Matriarch")
-		eq.spawn2(297210, 0, 0, x, y + 5, z, h) -- a_cragbeast_hatchling
-		eq.spawn2(297210, 0, 0, x + 5, y, z, h) -- a_cragbeast_hatchling
-		eq.spawn2(297210, 0, 0, x - 5, y, z, h) -- a_cragbeast_hatchling
-	elseif e.timer == "ae" then
-		eq.stop_timer(e.timer)
-		local target = e.self:GetTarget()
-		if target.valid then
-			e.self:CastSpell(1239, target:GetID()) -- Devouring Conflagration
-		end
-		eq.set_timer(e.timer, math.random(5, 60) * 1000)
-	elseif e.timer == "spawnevent" then
-		eq.stop_timer("spawnevent")
-		eq.spawn_condition("txevu", instance_id, 3, 1) -- bearers and Mastruqs
-  elseif e.timer == "absorbtion_r" then
-    eq.spawn2(297173, 0, 0, 
+  bearer_list = {297173, 297055, 297190, 297051, 297054, 297050, 297208, 297142, 297053, 297052, 297141, 297191}
+	for i = 1, #bearer_list do
+		eq.depop_all(bearer_list[i])
+    eq.signal(matriarch, bearer_list[i] + 1000000)
 	end
 end
 
-function ACMCombat(e)
+function init_event()
+	eq.spawn2(handler, 0, 0, unpack(handler_n)) --North Handler 
+	eq.spawn2(handler, 0, 0, unpack(handler_s)) --South Handler
+	sw = pick_bearer(bearer_sw) --Southwest Bearer
+	s = pick_bearer(bearer_s) --South Bearer
+	se = pick_bearer(bearer_se) --Southeast Bearer
+	nw = pick_bearer(bearer_nw) --Northwest Bearer
+	n = pick_bearer(bearer_n) --North Bearer
+	ne = pick_bearer(bearer_ne) --Northeast Bearer
+end
+
+function bearer_spawn(e)
+	eq.signal(matriarch, e.self:GetNPCTypeID())
+end
+
+function bearer_death(e)
+	if sw == e.self:GetNPCTypeID() then
+		sw = 0
+		eq.signal(matriarch, 1)
+	elseif s == e.self:GetNPCTypeID() then
+		s = 0
+		eq.signal(matriarch, 2)
+	elseif se == e.self:GetNPCTypeID() then
+		se = 0
+		eq.signal(matriarch, 3)
+	elseif nw == e.self:GetNPCTypeID() then
+		nw = 0
+		eq.signal(matriarch, 4)
+	elseif n == e.self:GetNPCTypeID() then
+		n = 0
+		eq.signal(matriarch, 5)
+	elseif ne == e.self:GetNPCTypeID() then
+		ne = 0
+		eq.signal(matriarch, 6)
+	end
+	eq.signal(matriarch, e.self:GetNPCTypeID() + 1000000)
+  table.insert(bearer_list, e.self:GetNPCTypeID())
+end
+
+function handler_spawn(e)
+	eq.set_timer('leash', 3 * 1000)
+end
+
+function handler_death(e)
+	eq.stop_timer('leash')
+end
+
+function handler_timer(e)
+	if e.timer == 'leash' then
+		if e.self:GetX() >= 700 then
+			e.self:GotoBind()
+			e.self:WipeHateList()
+		end
+	end
+end
+
+function matriarch_death(e)
+	-- Despawn everything because the players won
+	eq.stop_all_timers()
+	reset()
+end
+
+function matriarch_combat(e)
 	if e.joined then
 		combat = true
 		if adds then
-			eq.set_timer("hatchling", 35 * 1000)
+			eq.set_timer('hatchlings', 35 * 1000)
 		end
 		if ae then
-			eq.set_timer("ae", math.random(5, 60) * 1000)
+			eq.set_timer('aoe', math.random(5, 60) * 1000)
 		end
+		eq.stop_timer("reset")
 	else
 		combat = false
-		eq.stop_timer("hatchling")
-		eq.stop_timer("ae")
+		eq.stop_timer("hatchlings")
+		eq.stop_timer("aoe")
+		eq.set_timer("reset", 2 * 1000)
 	end
 end
 
--- This is less than ideal IMO, but since we can't keep around a pointer that
--- gets cleared in every case (#repop) we gotta rely on signals
-function ACMSignal(e)
-	if e.signal == 297173 then
+function matriarch_timer(e)
+	if e.timer == "hatchlings" then
+		local x,y,z,h = e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading();
+		eq.zone_emote(MT.Yellow, "In search of food, cragebeast hatchlings scurry out from beneath the Ancient Cragbeast Matriarch")
+		eq.spawn2(hatchling, 0, 0, x, y + 5, z, h)
+		eq.spawn2(hatchling, 0, 0, x + 5, y, z, h)
+		eq.spawn2(hatchling, 0, 0, x + 5, y + 5, z, h)
+	elseif e.timer == 'aoe' then
+		eq.stop_timer('aoe')
+		local target = e.self:GetTarget()
+		if target.valid then
+			e.self:CastSpell(1239, target:GetID()) --Devouring Conflagration
+		end
+	elseif e.timer == 'sw' then
+		eq.stop_timer('sw')
+		sw = pick_bearer(bearer_sw)
+	elseif e.timer == 's' then
+		eq.stop_timer('s')
+		s = pick_bearer(bearer_s)
+	elseif e.timer == 'se' then
+		eq.stop_timer('se')
+		se = pick_bearer(bearer_se)
+	elseif e.timer == 'nw' then
+		eq.stop_timer('nw')
+		nw = pick_bearer(bearer_nw)
+	elseif e.timer == 'n' then
+		eq.stop_timer('n')
+		n = pick_bearer(bearer_n)
+	elseif e.timer == 'ne' then
+		eq.stop_timer('ne')
+		ne = pick_bearer(bearer_ne)
+	elseif e.timer == 'reset' then
+    eq.stop_timer('reset')
+    e.self:CastSpell(3230, e.self:GetID())
+		e.self:WipeHateList()
+    reset()
+    eq.set_timer('respawn', 2 * 1000)
+  elseif e.timer == 'respawn' then
+    eq.stop_timer('respawn')
+    init_event()
+	end
+end
+
+function matriarch_signal(e)
+	local repop = 5 * 60 * 1000
+	if e.signal == 1 then
+		eq.set_timer('sw', repop)
+	elseif e.signal == 2 then
+		eq.set_timer('s', repop)
+	elseif e.signal == 3 then
+		eq.set_timer('se', repop)
+	elseif e.signal == 4 then
+		eq.set_timer('nw', repop)
+	elseif e.signal == 5 then
+		eq.set_timer('n', repop)
+	elseif e.signal == 6 then
+		eq.set_timer('ne', repop)
+	elseif e.signal == 297173 then
 		e.self:ModSkillDmgTaken(0, -75) -- 1h blunt
 		e.self:ModSkillDmgTaken(1, -75) -- 1h slashing
 		e.self:ModSkillDmgTaken(2, -75) -- 2h blunt
@@ -106,7 +201,7 @@ function ACMSignal(e)
 		e.self:ModSkillDmgTaken(38, -75) -- round kick
 		e.self:ModSkillDmgTaken(52, -75) -- tiger claw
 		e.self:ModSkillDmgTaken(74, -75) -- frenzy
-	elseif e.signal == 9297173 then
+	elseif e.signal == 1297173 then
 		e.self:ModSkillDmgTaken(0, 0) -- 1h blunt
 		e.self:ModSkillDmgTaken(1, 0) -- 1h slashing
 		e.self:ModSkillDmgTaken(2, 0) -- 2h blunt
@@ -123,124 +218,88 @@ function ACMSignal(e)
 		e.self:ModSkillDmgTaken(38, 0) -- round kick
 		e.self:ModSkillDmgTaken(52, 0) -- tiger claw
 		e.self:ModSkillDmgTaken(74, 0) -- frenzy
-    eq.set_timer("absorbtion_r", 5 * 60 * 1000)
 	elseif e.signal == 297055 then
 		e.self:SetSpecialAbility(SpecialAbility.rampage, 1)
 		e.self:SetSpecialAbilityParam(SpecialAbility.rampage, 0, 27)
-	elseif e.signal == 9297055 then
+	elseif e.signal == 1297055 then
 		e.self:SetSpecialAbility(SpecialAbility.rampage, 0)
-    eq.set_timer("anger_r", 5 * 60 * 1000)
 	elseif e.signal == 297190 then
 		e.self:SetSpecialAbility(SpecialAbility.flurry, 1)
 		e.self:SetSpecialAbilityParam(SpecialAbility.flurry, 0, 50)
-	elseif e.signal == 9297190 then
+	elseif e.signal == 1297190 then
 		e.self:SetSpecialAbility(SpecialAbility.flurry, 0)
-    eq.set_timer('haste_r', 5 * 60 * 1000)
 	elseif e.signal == 297051 then
 		e.self:ModifyNPCStat("min_hit", "1150")
 		e.self:ModifyNPCStat("max_hit", "4000")
-	elseif e.signal == 9297051 then
+	elseif e.signal == 1297051 then
 		e.self:ModifyNPCStat("min_hit", "850")
 		e.self:ModifyNPCStat("max_hit", "2750")
-    eq.set_timer("intensification_r", 5 * 60 * 1000)
 	elseif e.signal == 297054 then
 		e.self:ModifyNPCStat("combat_hp_regen", "6000")
-	elseif e.signal == 9297054 then
+	elseif e.signal == 1297054 then
 		e.self:ModifyNPCStat("combat_hp_regen", "100")
-    eq.set_timer("mending_r")
 	elseif e.signal == 297050 then
 		adds = true
 		if combat then
 			eq.set_timer("hatchling", 35000)
 		end
-	elseif e.signal == 9297050 then
+	elseif e.signal == 1297050 then
 		adds = false
 		eq.stop_timer("hatchling")
-    eq.set_timer("nascency_r")
 	elseif e.signal == 297208 then
 		ae = true
 		if combat then
 			eq.set_timer("ae", math.random(5, 60) * 1000)
 		end
-	elseif e.signal == 9297208 then
+	elseif e.signal == 1297208 then
 		ae = false
 		eq.stop_timer("ae")
-    eq.set_timer("projection_r")
 	elseif e.signal == 297142 then
 		e.self:ModifyNPCStat("attack_delay", "12")
-	elseif e.signal == 9297142 then
+	elseif e.signal == 1297142 then
 		e.self:ModifyNPCStat("attack_delay", "17")
-    eq.set_timer('quickening_r')
 	elseif e.signal == 297053 then
 		e.self:SetSpecialAbility(SpecialAbility.area_rampage, 1)
 		e.self:SetSpecialAbilityParam(SpecialAbility.area_rampage, 0, 17)
-	elseif e.signal == 9297053 then
+	elseif e.signal == 1297053 then
 		e.self:SetSpecialAbility(SpecialAbility.area_rampage, 0)
-    eq.set_timer('rage_r')
 	elseif e.signal == 297052 then
 		e.self:AddAISpell(0, 1248, 1024, -1, 30, -1) -- Spiritual Echo, in combat buff
-	elseif e.signal == 9297052 then
+	elseif e.signal == 1297052 then
 		e.self:RemoveAISpell(1248) -- Spiritual Echo
-    eq.set_timer('reflection_r')
 	elseif e.signal == 297141 then
 		e.self:ModifyNPCStat("mr", "236")
 		e.self:ModifyNPCStat("fr", "236")
 		e.self:ModifyNPCStat("cr", "236")
 		e.self:ModifyNPCStat("pr", "236")
 		e.self:ModifyNPCStat("dr", "236")
-	elseif e.signal == 9297141 then
+	elseif e.signal == 1297141 then
 		e.self:ModifyNPCStat("mr", "136")
 		e.self:ModifyNPCStat("fr", "136")
 		e.self:ModifyNPCStat("cr", "136")
 		e.self:ModifyNPCStat("pr", "136")
 		e.self:ModifyNPCStat("dr", "136")
-    eq.set_timer('resistance_r', 5 * 60 * 1000)
 	elseif e.signal == 297191 then
 		e.self:AddAISpell(0, 1249, 1024, -1, 30, -1) -- Bristling Armament, in combat buff
-	elseif e.signal == 9297191 then
+	elseif e.signal == 1297191 then
 		e.self:RemoveAISpell(1249) -- Bristling Armament
-    eq.set_timer('shielding_r', 5 * 60 * 1000)
-	end
+  end
 end
-
-function bearer_spawn(e)
-	eq.signal(297056, e.self:GetNPCTypeID());
-end
-
-function bearer_death(e)
-	eq.signal(297056, tonumber("9"..e.self:GetNPCTypeID()));
-end
-
-function ACHSpawn(e)
-	eq.set_timer("leash", 3 * 1000);
-end
-
-function ACHDeath(e)
-	eq.stop_timer('leash');
-end
-
-function ACHTimer(e)
-	if e.timer == "leash" then
-		if e.self:GetX() >= 700 then
-			e.self:GotoBind();
-			e.self:WipeHateList();
-		end
-	end
-end
-
+		
 function event_encounter_load(e)
-	eq.register_npc_event("acm", Event.spawn,				297056, ACMSpawn)
-	eq.register_npc_event("acm", Event.death_complete,		297056, ACMDeath)
-	eq.register_npc_event("acm", Event.timer,				297056, ACMTimer)
-	eq.register_npc_event("acm", Event.combat,				297056, ACMCombat)
-	eq.register_npc_event("acm", Event.signal,				297056, ACMSignal)
-
+	eq.register_npc_event("acm", Event.signal, matriarch, matriarch_signal)
+	eq.register_npc_event("acm", Event.timer, matriarch, matriarch_timer)
+	eq.register_npc_event("acm", Event.combat, matriarch, matriarch_combat)
+	eq.register_npc_event("acm", Event.death, matriarch, matriarch_death)
+	
+	eq.register_npc_event("acm", Event.timer, handler, handler_timer)
+	eq.register_npc_event("acm", Event.death, handler, handler_death)
+	eq.register_npc_event("acm", Event.spawn, handler, handler_spawn)
+	
 	for i = 1, #bearer_list do
-		eq.register_npc_event("acm", Event.spawn,			bearer_list[i], bearer_spawn)
-		eq.register_npc_event("acm", Event.death_complete,	bearer_list[i], bearer_death)
-	end
-
-	eq.register_npc_event("acm", Event.spawn,				297210, ACHSpawn)
-	eq.register_npc_event("acm", Event.timer,				297210, ACHTimer)
-	eq.register_npc_event("acm", Event.death_complete,		297210, ACHDeath)
+		eq.register_npc_event("acm", Event.death, bearer_list[i], bearer_death)
+		eq.register_npc_event("acm", Event.spawn, bearer_list[i], bearer_spawn)
+  end
+  reset()
+  init_event()
 end
